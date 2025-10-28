02950fcc508b042638590af4dec2c0b99dfd1fd6
Radek <radek.dumin@gmail.com>
2025-10-28 19:49:00 +0100

    fixes cd...

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,9 +1,14 @@
-import posthog from "posthog-js"
+import posthog from "posthog-js";
 
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: "/ingest",
-  ui_host: "https://eu.posthog.com",
-  defaults: '2025-05-24',
-  capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
-  debug: process.env.NODE_ENV === "development",
-});
+const key = process.env.NEXT_PUBLIC_POSTHOG_KEY;
+const apiHost = process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://eu.i.posthog.com";
+const uiHost = process.env.NEXT_PUBLIC_POSTHOG_UI_HOST || "https://eu.posthog.com";
+
+if (typeof window !== "undefined" && key) {
+  posthog.init(key, {
+    api_host: apiHost,
+    ui_host: uiHost,
+    capture_exceptions: true, // Enables capturing exceptions using Error Tracking
+    debug: process.env.NODE_ENV === "development",
+  });
+}
